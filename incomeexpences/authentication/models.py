20f53a4c7775bf73709ad3057222449254072d63
rukiyearican <rukiyearican@gmail.com>
2023-10-18 14:43:18 +0300

    added custom user model

--- incomeexpences/authentication/models.py
+++ incomeexpences/authentication/models.py
@@ -1,33 +1,61 @@
 from django.db import models
 
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 # Create your models here.
 
+
 class UserManager(BaseUserManager):
     
-    def create_user(self, username, email, password=None):
-        
+    def create_user(self, username, email, password = None):
         if username is None:
             raise TypeError("Users should have a username")
-        
+
         if email is None:
             raise TypeError("Users should have an email")
-        
-        user=self.model(username=username, email=self.normalize_email(email))
+
+        user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
         user.save()
-        
+
         return user
-        
-    def create_superuser(self, username, email, password=None):
-        
+
+    def create_superuser(self, username, email, password = None):
         if password is None:
             raise TypeError("password should not be none")
 
-        user=self.create_user(username, email, password)
+        user = self.create_user(username, email, password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
-        
+
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # define what attribute the user will be using to log in
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        if self.email is not None:
+            return self.email
+        return TypeError("Users should have an email")
+    
+    def tokens(self):
+        return 
